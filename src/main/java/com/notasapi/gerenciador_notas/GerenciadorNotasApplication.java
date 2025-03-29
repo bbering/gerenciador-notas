bbc5d755066115886aa5b689840e02d59f4cc9c5
Breno Bering <bbering88@gmail.com>
2025-03-29 14:55:14 -0300

    dependencias spring boot

---
+++ src/main/java/com/notasapi/gerenciador_notas/GerenciadorNotasApplication.java
@@ -0,0 +1,13 @@
+package com.notasapi.gerenciador_notas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorNotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorNotasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notasapi/gerenciador_notas/GerenciadorNotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notasapi.gerenciador_notas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorNotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
